ad64bb8a73ad97b6eace2dd9d5ca2c64616af3f8
Zoran Stoilov <zoranstoilov@yahoo.com>
2019-04-19 22:25:42 +0100

    added core app

---
+++ core/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Movie, Person
+
+
+class MovieList(ListView):
+    model = Movie
+    paginate_by = 10
+
+
+class MovieDetail(DetailView):
+    model = Movie
+    queryset = Movie.objects.all_with_related_persons()
+
+
+class PersonDetail(DetailView):
+    queryset = Person.objects.all_with_prefetch_movies()
